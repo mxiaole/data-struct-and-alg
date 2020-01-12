9e8ae8e83e51b472c137868b0bf21a38e6cbdde2
cody <mengjiale6@126.com>
2020-01-12 15:40:19 +0800

    add bst

---
+++ src/com/cody/datastruct/tree/BST.java
@@ -0,0 +1,262 @@
+package com.cody.datastruct.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * 二叉搜索树
+ * 1. 对树的操作基本上都是使用递归方法
+ */
+public class BST<E extends Comparable<E>> {
+    // 数据节点的封装
+    private class Node {
+        public E e;
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    // 定义二叉搜索树的成员变量
+    private Node root;  // 树的根
+    private int size;  // 树中节点的个数
+
+    // 构造函数
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    // 求解树中的节点个数
+    public int size() {
+        return this.size;
+    }
+
+    // 判断树是不是空
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // 向树中添加节点
+    public void add(E e) {
+        root = add(root, e);
+    }
+
+    // 向以node为根的二分搜索树中插入元素e，递归算法
+    // 返回插入新节点后二分搜索树的根
+    private Node add(Node node, E e) {
+        if (node == null) {
+            size++;
+            return new Node(e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = add(node.right, e);
+        } else if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        }
+
+        return node;
+    }
+
+    // 判断二叉搜索树中是否存在某个值
+    public boolean contains(E e) {
+        boolean res = contains(root, e);
+
+        return res;
+    }
+
+    private boolean contains(Node node, E e) {
+        if (node == null) {
+            return false;
+        }
+        if (node.e.equals(e)) {
+            return true;
+        } else if (node.e.compareTo(e) < 0) {
+            return contains(node.right, e);
+        } else {
+            return contains(node.left, e);
+        }
+    }
+
+    // 前序遍历, 递归
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    // 前序遍历非递归
+    public void preOrderNR() {
+        if (root == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            System.out.println(cur.e);
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
+    }
+
+    // 层序遍历二叉树
+    public void levelOrder() {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node cur = queue.remove();
+            System.out.println(cur.e);
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+            if (cur.right != null) {
+                queue.add(cur.right);
+            }
+        }
+    }
+
+    // 查找树中的最小值
+    public E minimum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("Error");
+        }
+        Node minNode = minimum(root);
+
+        return minNode.e;
+    }
+
+    // 查找node为根的树中左子树为空的节点
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    // 查找树中的最大值
+    public E maximum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("error");
+        }
+
+        Node maxNode = maximum(root);
+
+        return maxNode.e;
+    }
+
+    private Node maximum(Node node) {
+        if (node.right == null) {
+            return node;
+        }
+        return maximum(node.right);
+    }
+
+    // 删除树中的最小值节点
+    public E removeMin() {
+        E ret = minimum();
+        root = removeMin(root);
+        return ret;
+    }
+
+    // 返回删除了最小值节点的之后的树的根节点
+    private Node removeMin(Node node) {
+        if (node == null) {
+            Node right = node.right;
+            node.right = null;
+
+            size--;
+            return right;
+        }
+
+        node.left = removeMin(node.left);
+
+        return node;
+    }
+
+    // 从二分搜索树中删除最大值所在节点
+    public E removeMax() {
+        E ret = maximum();
+        root = removeMax(root);
+        return ret;
+    }
+
+    // 删除掉以node为根的二分搜索树中的最大节点
+    // 返回删除节点后新的二分搜索树的根
+    private Node removeMax(Node node) {
+
+        if (node.right == null) {
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    // 删除二叉搜索树中的某个节点
+    public void remove(E e) {
+        root = remove(root, e);
+    }
+
+    private Node remove(Node node, E e) {
+        if (node == null) {
+            return null;
+        }
+
+        if (e.compareTo(node.e) > 0) {
+            node.right = remove(node.right, e);
+            return node;
+        } else if (e.compareTo(node.e) < 0) {
+            node.left = remove(node.left, e);
+            return node;
+        } else {
+            // 待删除节点左子树为空的情况
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+
+            // 待删除节点右子树为空的情况
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+
+            // 待删除节点左右子树均不为空的情况
+
+            // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+            // 用这个节点顶替待删除节点的位置
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+
+            return successor;
+        }
+    }
+}
